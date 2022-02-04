fe00f7eb53154a429fad47e11943dd71c1932d8b
mu-perori <mu17perori@gmail.com>
2022-02-04 18:06:07 +0900

    画像のパスを修正した

--- sigcc/annotation.js
+++ sigcc/annotation.js
@@ -9,7 +9,7 @@ const textRgb = [255, 204, 0];
 const title = coordinateData["title"];
 const pages = coordinateData["pages"];
 
-const imgPath = title + "/images/";
+const imgPath = "images/";
 
 // フラグ
 let isClicked = false; //コマが選択されているか否か
