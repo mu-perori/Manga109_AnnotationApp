2dc6ae4bf356c8bb902edd3d283dcb3611b8fbf5
mu <61280283+mu-perori@users.noreply.github.com>
2022-03-05 04:35:20 +0900

    フォーマッタのセットアップ (#3)
    * フォーマッタのセットアップ
    
    * 設定ファイルの修正
    
    * Pythonのコードをフォーマットした

--- build/modules/setting_dirs.py
+++ build/modules/setting_dirs.py
@@ -1,18 +1,19 @@
 # 各作品ごとのフォルダ作る
 
 import os
-import glob#ファイル名を一度に取得するのに使う
+import glob  # ファイル名を一度に取得するのに使う
 
 # タイトルリストを作成して返す
 def get_titles(input_path):
-  xml_path = input_path + "/annotations/"
-  manga_title_paths = glob.glob(xml_path + "*.xml")
-  manga_titles = []
-  for title_path in manga_title_paths:
-    manga_title = title_path.replace(xml_path, "").replace(".xml", "")
-    manga_titles.append(manga_title)
-  return manga_titles
+    xml_path = input_path + "/annotations/"
+    manga_title_paths = glob.glob(xml_path + "*.xml")
+    manga_titles = []
+    for title_path in manga_title_paths:
+        manga_title = title_path.replace(xml_path, "").replace(".xml", "")
+        manga_titles.append(manga_title)
+    return manga_titles
 
-#漫画タイトルのフォルダを作成
+
+# 漫画タイトルのフォルダを作成
 def make_dirs(output_path, manga_title):
-  os.makedirs(output_path + "/" + manga_title, exist_ok=True)
+    os.makedirs(output_path + "/" + manga_title, exist_ok=True)
