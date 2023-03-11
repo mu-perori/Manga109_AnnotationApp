7334b95c0874bf3ca784ff80dc1f1ab3a57e6186
mu-perori <mu17perori@gmail.com>
2023-03-11 20:35:35 +0900

    packing.pyにcopy_html関数を追加

--- build/modules/packing.py
+++ build/modules/packing.py
@@ -6,11 +6,13 @@ import shutil
 def copy_annotation(output_path, src_path):
     shutil.copy(src_path + "/annotation.js", output_path)
 
-
 # index.cssを出力先にコピー
 def copy_css(output_path, src_path):
     shutil.copy(src_path + "/index.css", output_path)
 
+# annotation_tool.htmlを出力先にコピー
+def copy_html(output_path, src_path):
+    shutil.copy(src_path + "/annotation_tool.html", output_path)
 
 # 各漫画の画像が入っているフォルダを漫画のタイトルからimagesにリネームして出力先にコピー
 def copy_img(input_path, output_path, manga_title):
